6dc39b271fb3a500ab266cdda634e8a18c6521b6
zeticous <e0004327@u.nus.edu>
2017-01-27 12:37:25 +0800

    T3A2:
    Encapsulated feedbackToUser

--- src/seedu/addressbook/commands/CommandResult.java
+++ src/seedu/addressbook/commands/CommandResult.java
@@ -11,8 +11,12 @@ import java.util.Optional;
 public class CommandResult {
 
     /** The feedback message to be shown to the user. Contains a description of the execution result */
-    public final String feedbackToUser;
-
+    private final String feedbackToUser;
+    
+    public String getFeedbackToUser(){
+        return feedbackToUser;
+    }
+    
     /** The list of persons that was produced by the command */
     private final List<? extends ReadOnlyPerson> relevantPersons;
 
