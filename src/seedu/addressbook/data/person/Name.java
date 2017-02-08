01c311b0fb3ed764c064eb6be0dc84bf6a99992c
zeticous <e0004327@u.nus.edu>
2017-02-08 22:09:17 +0800

    [T4A7] Completed method isSimilar, code refactored.

--- src/seedu/addressbook/data/person/Name.java
+++ src/seedu/addressbook/data/person/Name.java
@@ -45,23 +45,27 @@ public class Name {
             return false;
         }
         
-        HashMap<String,Integer> fullNameWordMap = new HashMap<String,Integer>();
+        String formattedFullName = formatNameToLowerCaseWithoutCommas(fullName);
+        String formattedOtherName = formatNameToLowerCaseWithoutCommas(other);
         
-        String fullNameWithoutCommas = fullName.replaceAll(","," ").toLowerCase();
-        String otherNameWithoutCommas = other.replaceAll(","," ").toLowerCase();
+        String[] fullNameFragments = createNameFragments(formattedFullName);
+        String[] otherNameFragments = createNameFragments(formattedOtherName);
         
-        String[] fullNameWords = fullNameWithoutCommas.split(" ");
-        String[] otherNameWords = otherNameWithoutCommas.split(" ");
+        HashMap<String,Integer> fullNameWordMap = intialiseNameWordMap(fullNameFragments);
         
-        for(String word: fullNameWords){
-            if(fullNameWordMap.containsKey(word)){
-                fullNameWordMap.put(word, fullNameWordMap.get(word)-1);
-            } else {
-                fullNameWordMap.put(word, 1);
-            }
-        }
-        
-        for(String word: otherNameWords){
+        return compareOtherToWordMap(otherNameFragments, fullNameWordMap);
+    }
+    
+    /**
+     * Compare the words in the other name in the word map.
+     * @param otherNameFragments: string array with individual words in 
+     *        lower case.
+     * @param fullNameWordMap: HashMap containing the individual words 
+     *        of the full name.
+     * @return
+     */
+    private boolean compareOtherToWordMap(String[] otherNameFragments, HashMap<String, Integer> fullNameWordMap) {
+        for(String word: otherNameFragments){
             if(fullNameWordMap.containsKey(word)){
                 if(fullNameWordMap.get(word)>1){
                     fullNameWordMap.put(word,fullNameWordMap.get(word)-1);
@@ -72,7 +76,44 @@ public class Name {
                 return false;
             }
         }
+        
         return true;
+    }
+
+    /**
+     * Creates the hashmap for comparison of the name.
+     * @param string array of individual words in lower casing.
+     * @return hashmap of the words in the array.
+     */
+    private HashMap<String,Integer> intialiseNameWordMap(String[] fullNameFragments) {
+        HashMap<String,Integer> fullNameWordMap = new HashMap<String,Integer>();
+        
+        for(String word: fullNameFragments){
+            if(fullNameWordMap.containsKey(word)){
+                fullNameWordMap.put(word, fullNameWordMap.get(word)-1);
+            } else {
+                fullNameWordMap.put(word, 1);
+            }
+        }
+        
+        return fullNameWordMap;
+    }
+    /**
+     * format the name for the isSimilar method.
+     * @param a string of the name.
+     * @return name in lower case with commas replaced with white space.
+     */
+    private String formatNameToLowerCaseWithoutCommas(String name){
+        return name.replace(",", " ").toLowerCase();
+    }
+    
+    /**
+     * Splits a name into its individual words.
+     * @param a string of the name with all commas replaced with whitespace.
+     * @return a string array containing all the individual words of the name.
+     */
+    private String[] createNameFragments(String formattedName){
+        return formattedName.split(" ");
     }
 
     /**
